675ddf888c4fe44de64d210a67e66476fc9be99c
Philipp Schirmer <philipp@schirmer-online.de>
2016-01-14 01:25:44 +0100

    sessions and messages for users

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'secret'}));
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- controllers/application.js
+++ controllers/application.js
@@ -4,7 +4,8 @@ var webParser = require('../modules/webParser');
 var baseUrl = "http://www.imdb.com/";
 
 module.exports.get_index = function(req, res, next) {
-  res.render('index', { title: 'IMDb Ratings' });
+  res.render('index', { title: 'IMDb Ratings', message: req.session.message });
+  req.session.message = null;
 }
 
 module.exports.get = function(req, res, next) {
@@ -26,6 +27,7 @@ module.exports.search = function(req, res, next) {
       if(!err && response.statusCode == 200) {
         res.redirect('/' + query);
       } else {
+        req.session.message = "Series not found";
         res.redirect('/');
       }
     })
@@ -35,6 +37,7 @@ module.exports.search = function(req, res, next) {
       function (err, window) {
         var series = window.$('#main > table > tbody > tr:nth-child(2) > td.title > a');
         if(series.length == 0) {
+          req.session.message = "No results";
           res.redirect('/');
         } else {
           var regex = /title\/(.+)\//
